4a23af7cdc7378b1c4ae353d77d97a8ea639c93e
FRANCIS ROSARIO <FRANCISMICO.ROSARIO@BENILDE.EDU.PH>
2021-07-31 03:47:39 +0800

    IAction.java: Update interface for xumm4j

--- src/main/java/com/xwtxumm/xummnftwalletcreator/api/IAction.java
+++ src/main/java/com/xwtxumm/xummnftwalletcreator/api/IAction.java
@@ -1,6 +1,8 @@
 package com.xwtxumm.xummnftwalletcreator.api;
 
-public interface Action {
+import java.io.IOException;
+
+public interface IAction {
     //////////////
     //Endpoints
     String payload_post = "https://xumm.app/api/v1/platform/payload";
@@ -11,7 +13,7 @@ public interface Action {
     String signIn_Desktop = "{\"options\":{\"submit\":\"true\",\"multisign\":\"false\",\"expire\":\"1440\",\"return_url\":{\"web\":\"https://xls19d-xumm-dev.herokuapp.com/\"}},\"txjson\":{\"TransactionType\":\"SignIn\"}}";
     String signIn_Phone = "{\"options\":{\"submit\":\"true\",\"multisign\":\"false\",\"expire\":\"1440\",\"return_url\":{\"app\":\"https://xls19d-xumm-dev.herokuapp.com/\"}},\"txjson\":{\"TransactionType\":\"SignIn\"}}";
 
-    void processAuthorization();
-    void checkAuthorization();
-    void createNFTWallet(byte[] imageByte, String nftName, String nftAuthor, String nftEmail, String nftTwitter, String nftDescription);
+    void processAuthentication();
+    void checkAuthentication() throws IOException;
+    void createNFTWallet(byte[] imageByte, String nftName, String nftAuthor, String nftEmail, String nftTwitter, String nftDescription) throws IOException;
 }
